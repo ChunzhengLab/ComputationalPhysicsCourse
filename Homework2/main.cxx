c55a2189ca9062fd7fb8ebae6341082f19ba9312
ChunzhengLab <chunzheng.wang@icloud.com>
2022-10-23 16:27:17 +0800

    format

--- Homework2/main.cxx
+++ Homework2/main.cxx
@@ -14,7 +14,7 @@ double DoIntegral(FunctionType fuc, double lowlim, double upplim, double err) {
   int n = 1;
   double I1, I2;
   double h = upplim - lowlim;
-  I2 = h/2. * (fuc(lowlim) + fuc(upplim));
+  I2 = h / 2. * (fuc(lowlim) + fuc(upplim));
   do {
     I1 = I2;
     I2 = 0;
@@ -22,15 +22,14 @@ double DoIntegral(FunctionType fuc, double lowlim, double upplim, double err) {
     n *= 2;
     double xk = lowlim;
     for (size_t i = 0; i < n; i++) {
-      I2 += 0.5* h * (fuc(xk) + fuc(xk + h));
+      I2 += 0.5 * h * (fuc(xk) + fuc(xk + h));
       xk += h;
     }
   } while (fabs(I2 - I1) > err);
   return I2;
 }
 
-
 int main() {
-  double ans = DoIntegral(fuc,lower_limit,upper_limit,err);
+  double ans = DoIntegral(fuc, lower_limit, upper_limit, err);
   cout << "result is " << ans << endl;
 }
